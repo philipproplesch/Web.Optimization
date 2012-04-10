a514c97159a04aa8c3c8cf5d46ca8bd77eb9c9fc
Philip Proplesch <philip@proplesch.de>
2012-04-10 13:49:50 +0200

    Increment and set package version during build process.

--- bld/default.ps1
+++ bld/default.ps1
@@ -3,6 +3,7 @@ $src = Join-Path $base 'src'
 $pkg = Join-Path $base 'pkg'
 $out = Join-Path $base 'out'
 $nuget = "$src\.nuget\NuGet.exe"
+$version = "0.0.0"
 
 Task Default -depends Initialize-Directories, Create-NuGetPackages
 
@@ -14,16 +15,44 @@ Task Initialize-Directories {
   New-Item $out -Type directory  
 }
 
-Task Create-NuGetPackages -depends Build {
-	Push-Location $out
-	Get-ChildItem $pkg -Filter *.nuspec | ForEach {
-	  $spec = $_.FullName
-	  Exec { Invoke-Expression "$nuget pack $spec" }
-
-	}
-	Pop-Location
+Task Create-NuGetPackages -depends Build, Set-PackageVersion {
+  Push-Location $out
+  
+  Get-ChildItem $pkg -Filter *.nuspec | ForEach {
+    $spec = $_.FullName
+    Exec { Invoke-Expression "$nuget pack $spec" }
+  }
+  
+  Pop-Location
 }
 
 Task Build {
-	Exec { msbuild "$src\Web.Optimization.sln" /t:Build /p:Configuration=Release }
+  #Exec { msbuild "$src\Web.Optimization.sln" /t:Build /p:Configuration=Release }
+}
+
+Task Set-PackageVersion {
+  $file = "version.xml"
+  $xml = [xml](Get-Content $file)
+
+  [string] $suffix = $xml.version.suffix
+  [int] $major = $xml.version.major
+  [int] $minor = $xml.version.minor
+  [int] $patch = [int] $xml.version.patch + 1
+
+  $xml.version.patch = [string] $patch
+  $xml.Save($file)
+
+  if ($suffix.Length -gt 0) {
+    $version = "$major.$minor.$patch-$suffix"
+  }
+  else {
+    $version = "$major.$minor.$patch"
+  }
+  
+  # Set NuGet package version.
+  Get-ChildItem $pkg -Filter *.nuspec | ForEach {
+    $spec = [xml](Get-Content $_.FullName)
+    $spec.package.metadata.version = $version
+	$spec.Save($_.FullName)
+  }
 }
