408abf1973afff2d551572d25a6224d99a464169
Philip Proplesch <philip@proplesch.de>
2012-04-13 21:53:41 +0200

    Fixed name.

--- src/Web.Optimization/Extensions/BundleTransformExtensions.cs
+++ src/Web.Optimization/Extensions/BundleTransformExtensions.cs
@@ -5,7 +5,7 @@ namespace Web.Optimization.Extensions
 {
     public static class BundleTransformExtensions
     {
-        // Stolen from @howarddierking ;) => https://gist.github.com/1998379
+        // Stolen from @@howard_dierking ;) => https://gist.github.com/1998379
 
         public static IBundleTransform Then(
             this IBundleTransform instance, IBundleTransform then)
