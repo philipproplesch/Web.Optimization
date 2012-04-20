942f2646f7036de2c26360d194fe0d61e104c66c
Philip Proplesch <philip@proplesch.de>
2012-04-20 22:24:45 +0200

    Resolve LESS imports.

--- src/Web.Optimization.Bundles.Less/LessTransform.cs
+++ src/Web.Optimization.Bundles.Less/LessTransform.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Optimization;
+﻿using System.Configuration;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web.Optimization;
 using Web.Optimization.Common;
 
 namespace Web.Optimization.Bundles.Less
@@ -7,8 +11,50 @@ namespace Web.Optimization.Bundles.Less
     {
         public void Process(BundleContext context, BundleResponse response)
         {
+            var builder = new StringBuilder();
+
+            foreach (var file in response.Files)
+            {
+                if (!File.Exists(file.FullName))
+                    continue;
+
+                var content = ResolveImports(file);
+                builder.AppendLine(dotless.Core.Less.Parse(content));
+            }
+
             response.ContentType = ContentTypes.Css;
-            response.Content = dotless.Core.Less.Parse(response.Content);
+            response.Content = builder.ToString();
+        }
+
+        private static readonly Regex s_lessImport = 
+            new Regex("@import [\"|'](.+)[\"|'];", RegexOptions.Compiled);
+
+        private static string ResolveImports(FileInfo file)
+        {
+            var content = File.ReadAllText(file.FullName, Encoding.UTF8);
+
+            content =
+                s_lessImport.Replace(
+                    content,
+                    match =>
+                        {
+                            var name = match.Groups[1].Value;
+
+                            var import =
+                                Path.Combine(
+                                    file.Directory.FullName,
+                                    name);
+
+                            if (!File.Exists(import))
+                            {
+                                throw new ConfigurationErrorsException(
+                                    string.Concat("Unable to resolve import ", name));
+                            }
+
+                            return match.Value.Replace(name, import);
+                        });
+
+            return content;
         }
     }
 }
